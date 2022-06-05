e062e952ea4f4359eed1d12496fc8520787284ba
Cesar Palacios <cesarlp.ng@gmail.com>
2022-06-05 10:59:30 -0300

    Chapter 2

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 mod chapter1;
+mod chapter2;
 mod chapter3;
 
+// comment the exercises that you don't want to run
 fn main() {
   chapter1::execute();
+  chapter2::execute();
   chapter3::execute();
 } 
